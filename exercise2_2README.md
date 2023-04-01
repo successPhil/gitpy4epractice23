507894a35525ddc2dd29562680aae93a2a2a91af
successPhil <129565748+successPhil@users.noreply.github.com>
2023-03-31 23:30:32 -0400

    'third commit'

--- exercise2_2README.md
+++ exercise2_2README.md
@@ -1,4 +1,3 @@
 #exercise2.2
 I am creating a repository of py4e exercises to practice using gitHub.
 
-The prompt for exercise 2.2: Use the input() function to prompt the user to enter their name. Greet the user with "Hello" and the users name.
--- lesson3_exercise2_2.py
+++ lesson3_exercise2_2.py
@@ -1,3 +1,5 @@
-# initial setup
+#The prompt for exercise 2.2: Use the input() 
+#function to prompt the user to enter their name. 
+#Greet the user with "Hello" and the users name. 
 name = input('Enter your name:')
 print(f'Welcome, {name}!!')
